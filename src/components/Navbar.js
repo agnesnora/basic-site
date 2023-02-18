0b83f6e2971c2d28796a607eac63813d0e53766d
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-02-18 13:56:51 +0100

    delpoy

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-left">
+        <img className="nav-img" src="./logo512.png" alt="logo" />
+        <h4>ReactFacts</h4>
+      </div>
+      <p>React Course - Project 1</p>
+    </nav>
+  );
+}
